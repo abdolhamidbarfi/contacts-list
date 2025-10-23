45bf78e4f1e945ad069ba8cbe14a7c64eba2abb9
Hamid Barfi <barfiabdolhamid@gmail.com>
2025-10-23 05:51:05 +0330

    chore: add custom font to the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,14 +4,9 @@ import localFont from "next/font/local";
 import "@/styles/globals.css";
 import Header from "@/components/Header";
 
-const geistSans = localFont({
-  src: "../styles/fonts/GeistVF.woff",
+const yekanBakhRegular = localFont({
+  src: "../styles/fonts/YekanBakh-Regular.woff",
   variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "../styles/fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
   weight: "100 900",
 });
 
@@ -27,9 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fa" dir="rtl">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${yekanBakhRegular.className}  antialiased`}>
         <StoreProvider>
           <Header />
           {children}
